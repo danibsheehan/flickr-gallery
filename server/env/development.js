306a651bbcbd47293c57e007ea1321becccbe974
Danielle Sheehan <danibsheehan@gmail.com>
2016-09-02 19:46:20 -0400

    deploying to heroku

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "postgres://localhost:5432/fsg",
+  "DATABASE_URI": process.env.DATABASE_URL,
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "API_KEY":"1234",
   "LOGGING": true
